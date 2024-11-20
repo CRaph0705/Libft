e4572aa5d0edacec5e39c314536bfd709501b7f8
Raphael Cochrane <rcochran@paul-f3Ar2s5.clusters.42paris.fr>
2024-11-20 15:01:33 +0100

    trim wip

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:58:56 by rcochran          #+#    #+#             */
-/*   Updated: 2024/11/19 18:03:30 by rcochran         ###   ########.fr       */
+/*   Updated: 2024/11/20 14:57:12 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	str_start;
 	unsigned int	str_end;
-	unsigned int	i;
 	size_t			str_len;
 	char			*str;
 
@@ -41,24 +40,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
-	i = 0;
 	str_start = 0;
-	str_end = (unsigned int)ft_strlen((char *)s1);
-	while (s1[i] && is_in_array(s1[i], (char *)set) == 1)
-	{
+	while (s1[str_start] && is_in_array(s1[str_start], (char *)set) == 1)
 		str_start++;
-		i++;
-	}
-	i = (unsigned int)ft_strlen((char *)s1) - 1;
-	while (s1[i] && str_end > str_start && is_in_array(s1[i], (char *)set) == 1)
-	{
+	str_end = (unsigned int)ft_strlen((char *)s1) - 1;
+	while (*s1 && str_end > str_start
+		&& is_in_array(s1[str_end], (char *)set) == 1)
 		str_end--;
-		i--;
-	}
-	str_len = (size_t)(str_end - str_start);
+	str_len = (size_t)(str_end - str_start) + 1;
 	str = ft_substr(s1, str_start, str_len);
 	return (str);
 }
+
 /* int	main(int ac, char **av)
 {
 	(void)ac;
